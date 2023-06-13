b7cfe7110c7beff8811c8ff3e7a4153ef26ac6b6
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-06-13 15:45:35 -0300

    Navbar pronta

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,16 @@
+import { Container } from "./styled.tsx";
+
 function Header() {
   return (
     <>
-        <header>
-            <nav>
-            </nav>
-        </header>
+      <Container>
+        <nav className="flex justify-around items-center">
+          <h1 className="text-3xl"> <strong className="text-newGreen">e</strong>Front</h1>
+          <button className="border-2 rounded-md border-newGreen p-2 font-semibold w-40 h-12">comprar ebook</button>
+        </nav>
+      </Container>
     </>
-  )
+  );
 }
 
 export default Header;
---
+++ src/components/header/styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  padding: 10px 70px;
+  width: 100vw;
+  height: 10vh;
+  margin-top: 10px;
+  color: #fff;
+`;
